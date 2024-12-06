61e3b678457e7eec302135854cc8408ead6b1be6
Munawwar <munawwarfiroz@hotmail.com>
2024-12-06 16:26:03 +0400

    accept "&" sign in branch names

--- merges.sh
+++ merges.sh
@@ -17,7 +17,7 @@ printf "\nMerged branches:\n"
 # try to remove surrounding text to get only branch names
 # remove duplicates
 git log --format="%s" --merges --reverse origin/master..origin/$1 | cat - \
-  | sed -E "s~Merge branch '([-a-zA-Z0-9]+)' into .+~\1~" \
-  | sed -E "s~Merge (remote-tracking )?branch '.+/([-\.a-zA-Z0-9]+)' into .+~\2~" \
+  | sed -E "s~Merge branch '([^/]+)' into .+~\1~" \
+  | sed -E "s~Merge (remote-tracking )?branch '.+/(.+)' into .+~\2~" \
   | sed -E "s~Merge pull request #[0-9]+ from Carriyo/(.+)~\1~" \
   | awk '!x[$0]++'
